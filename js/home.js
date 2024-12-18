05bfabc017cc976df6ca36e288a69a319f3d32fd
Emon <abamin3091@gmail.com>
2024-12-18 21:35:16 +0600

    added pertially the add event listener

--- js/home.js
+++ js/home.js
@@ -1,1 +1,25 @@
-console.log('home added')
+// add money button to account
+
+
+// s-1 : add event handler
+// prevent page reload 
+
+// s-2 : get money to be added to the account balance
+
+
+// step -01: add an event handler in added button inside the form
+
+document.getElementById('add-money-btn')
+.addEventListener('click',function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+  // step-2 : get money to be added to the account balance
+
+  const addMoneyInput =document.getElementById('input-add-money').value;
+  console.log(addMoneyInput);
+
+  // get the pin number provided
+
+   const pinNumberInput = document.getElementById('input-pin-number').value;
+   console.log(pinNumberInput)
+})
